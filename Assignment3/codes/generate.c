e7add6ed41ecea978014b39287b2c45a0972e42e
Swaraj528 <sharmaswaraj528@gmail.com>
2024-09-02 23:44:09 -0700

    assignment 3 initial commit

---
+++ Assignment3/codes/generate.c
@@ -0,0 +1,51 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "libs/matfun.h"
+#include "libs/geofun.h"
+
+int main() {
+	double **k, **l, **M, **P, **Q;
+	int ax=-2,ay=0,bx=0,by=8;
+	M = createMat(2,2);
+	k = createMat(2,1);
+	l = createMat(2,1);
+	P = createMat(2,1);
+	Q = createMat(2,1);
+	M[0][1] = ax;
+	M[1][1] = ay;
+	M[0][0] = bx;
+	M[1][0] = by;
+	k[0][0] = (float) 2/3;
+	k[1][0] = (float) 1/3;
+	l[0][0] = (float) 1/3;
+	l[1][0] = (float) 2/3;
+	P = Matmul(M,l,2,2,1);
+	Q = Matmul(M,k,2,2,1);
+	FILE *file;
+	file = fopen("values.dat", "w");
+
+	if (file == NULL) {
+		printf("Error opening file!\n");
+		return 1;
+	}
+	fprintf(file, "x\ty\t of P and then Q\n");
+	fprintf(file, "%.02lf\t", P[0][0]);
+	fprintf(file, "%.02lf\t", P[1][0]);                                                                                                
+        fprintf(file, "%.02lf\t", Q[0][0]);                                                                                                                                                 
+        fprintf(file, "%.02lf\t", Q[1][0]);   
+	fclose(file);
+	printf("Results have been written to values.dat\n");
+
+	freeMat(M,2);
+	freeMat(k,2);
+	freeMat(l,2);
+	freeMat(P,2);
+	freeMat(Q,2);
+	return 0;
+}
---
+++ Assignment3/codes/plot.py
@@ -0,0 +1,70 @@
+#Code by GVV Sharma
+#July 22, 2024
+#released under GNU GPL
+#Line 
+
+
+import sys                                          #for path to external scripts
+sys.path.insert(0, '/home/gvt/Documents/sdcard/github/matgeo/codes/CoordGeo')        #path to my scripts
+import numpy as np
+import mpmath as mp
+import numpy.linalg as LA
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#local imports
+from line.funcs import *
+from triangle.funcs import *
+from conics.funcs import circ_gen
+
+
+data = np.loadtxt("values.dat", skiprows=1)  # Skip the header row
+
+xp = data[0]
+yp = data[1]
+xq = data[2]
+yq = data[3]
+#Given points
+A = np.array(([-2, 0])).reshape(-1,1) 
+B = np.array(([0, 8])).reshape(-1,1) 
+P = np.array(([xp, yp])).reshape(-1,1) 
+Q = np.array(([xq, yq])).reshape(-1,1)
+
+x_AB = line_gen(A,B)
+
+plt.plot(x_AB[0,:],x_AB[1,:],label='$distance(AB)$')
+
+colors = np.arange(1,5)
+#Labeling the coordinates
+quad_coords = np.block([A,B,P,Q])
+plt.scatter(quad_coords[0,:], quad_coords[1,:], c=colors)
+vert_labels = ['A','B','P','Q']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(f'{txt}\n({quad_coords[0,i]:.2f}, {quad_coords[1,i]:.2f})',
+                 (quad_coords[0,i], quad_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(10,-30), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+# use set_position
+ax = plt.gca()
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+'''
+ax.spines['left'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+'''
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+plt.title('Finding the points of trisection of A and B', loc = 'right', pad = 15)
+plt.grid() # minor
+plt.axis('equal')
+plt.savefig('/home/gvt/Documents/sdcard/github/EE1030/Assignment3/figs/fig1.png')
+plt.show()
